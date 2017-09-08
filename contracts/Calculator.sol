d348f7f15a97d3715502a37a27b528f044fef563
Tyler Brown <tbrown93@gatech.edu>
2017-09-07 23:54:44 -0400

    Re-added FactorSet event on Calculator

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.11;
 
 contract Calculator {
 
+    event FactorSet(address indexed sender, uint newFactor);
+
     function calc(uint num) public returns (uint);
     function getFactor() public returns (uint);
     function setFactor(uint newFactor);
